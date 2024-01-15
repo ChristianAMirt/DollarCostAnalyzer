3a923be8732537ea81fe364fe10f23983bd4219e
Christian Mirt <cmirt@cox.net>
2024-01-14 23:16:05 -0500

    Added cutoff date function and __str__ to test API

--- DollarCostAnalyzer.py
+++ DollarCostAnalyzer.py
@@ -1,10 +1,13 @@
 import sys
 
-from IntervalAnalyzer import IntervalAnalyzer
+#from IntervalAnalyzer import IntervalAnalyzer
 from StockBuilder import StockBuilder
 
 
 def main():
+    test_stock = StockBuilder('SPY', "2023-01-13")
+
+    print(test_stock)
 
 if __name__ == "__main__":
     try:
--- StockBuilder.py
+++ StockBuilder.py
@@ -1,14 +1,18 @@
-from typing import List
 import requests
+import json
 
+#Will be used to stop data if no date is provided
+DEFAULT_DATE = "2004-01-14"
 
 class StockBuilder:
     """
     Sends requests to AlphaVantage API and processes response into Stock object
     """
-    def __init__(self, ticker: str = "No-Name"):
+    def __init__(self, ticker: str = "No-Name", date: str = DEFAULT_DATE):
         self._ticker_symbol: str = ticker
-        self._json_data: dict = read_from_API()
+        self._end_date_of_data: str = date
+        self._json_data: dict = self.read_from_API()
+
 
     @property
     def ticker_symbol(self) -> str:
@@ -17,13 +21,39 @@ class StockBuilder:
         """
         return self._ticker_symbol
     
+    @property
+    def end_date_of_data(self) -> str:
+        """
+        Get end date range of data
+        """
+        return self._end_date_of_data
+    
+    
     def read_from_API(self) -> dict:
         """
         Retrives data from stock
         """
-        url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={_ticker_symbol}&outputsize=full&apikey=XY1MFHCZCQT2EIN4'
+        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={self.ticker_symbol}&outputsize=full&apikey=XY1MFHCZCQT2EIN4'
         response = requests.get(url)
         if response.status_code == 200:
-            return response.json()
+            return self._cutoff_date(response.json())
         else: 
-            raise ValueError("API Response not Valid")
+            return KeyError
+        
+    def _cutoff_date(self, raw_data) -> dict:
+        """
+        Removes any data beyond the given date
+        """
+        new_data = {}
+
+        for date in raw_data["Time Series (Daily)"]:
+            if date <= self._end_date_of_data:
+                break
+            
+            new_data[date] = raw_data["Time Series (Daily)"][date]["1. open"]
+
+        return new_data
+
+    def __str__(self) -> str:
+        print(f"Data for the stock ticker: {self._ticker_symbol}", '\n')
+        return json.dumps(self._json_data, indent=2)
