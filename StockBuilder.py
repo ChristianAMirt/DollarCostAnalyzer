62e07b94e577a335dfc463584e68bbd080805b33
Christian Mirt <cmirt@cox.net>
2024-01-14 22:04:49 -0500

    Restructured and Implemented Class

--- StockBuilder.py
+++ StockBuilder.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from typing import List
 import requests
 
 
@@ -6,29 +6,24 @@ class StockBuilder:
     """
     Sends requests to AlphaVantage API and processes response into Stock object
     """
-    def __init__(self, ticker):
+    def __init__(self, ticker: str = "No-Name"):
+        self._ticker_symbol: str = ticker
+        self._json_data: dict = read_from_API()
 
+    @property
+    def ticker_symbol(self) -> str:
+        """
+        Get the symbol for the stock
+        """
+        return self._ticker_symbol
     
-    def read_from_API(self):
+    def read_from_API(self) -> dict:
         """
         Retrives data from stock
         """
-
-    def response_is_valid(self) -> bool:
-        """
-        Ensures API prodived a valid response or raises exception
-        """
-    def __iter__(self):
-        """
-        Interator for daily_Data_Collection
-        """
-    def __repr__(self):
-
-    @dataclass
-    class DailyData:
-        """
-        Price of stock for a specific date
-        """
-        date: str
-        open_price: float
-
+        url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={_ticker_symbol}&outputsize=full&apikey=XY1MFHCZCQT2EIN4'
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else: 
+            raise ValueError("API Response not Valid")
